0ece636a5323034b916117b88348636706d989a1
Christian Wakefield <CJOWakefield@outlook.com>
2025-02-27 15:04:18 +0800

    Readjusted for downloader.py changes. For testing.

--- scripts/download.py
+++ scripts/download.py
@@ -1,10 +1,22 @@
-from src.data import SatelliteDownloader
+import sys
+from pathlib import Path
+import asyncio
 
-def main():
-    downloader = SatelliteDownloader()
-    downloader.process_location(country="United Kingdom",
+project_root = str(Path(__file__).parent.parent)
+sys.path.append(project_root)
+
+from src.data.downloader import SatelliteDownloader
+
+async def main():
+    downloader = SatelliteDownloader(testing=True)
+    await downloader._init_session()
+    directory, images = await downloader.process_location(country="United Kingdom",
                                 city="London",
                                 postcode="SW1A 1AA")
+    
+    if directory and images:
+        downloader.save_to_final_location(directory)
+        print(f'\n{len(images)} images downloaded and saved to {directory}')
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
